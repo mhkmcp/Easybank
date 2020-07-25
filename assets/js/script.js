8a7483006e2cc375fe6a740ac915bc8cf3ba73e7
mhkmcp <mhkmcp@yahoo.com>
2020-07-25 23:05:49 +0600

    Added Responsive Menu

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const btnHamburger = document.querySelector("#btnhamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", function () {
+  console.log("open hamburger");
+  if (header.classList.contains("open")) {
+    // Close Hamburger
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach(function (element) {
+      element.classList.add("fade-out");
+      element.classList.remove("fade-in");
+    });
+    // overlay.classList.remove("fade-in");
+    // overlay.classList.add("fade-out");
+  } else {
+    // Open Hamburger
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+    // overlay.classList.add("fade-in");
+    // overlay.classList.remove("fade-out");
+  }
+});
